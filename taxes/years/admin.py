8db929b3f4aa13242a28a12a0c85e5e60a7ad14b
Regie-Daquioag <regiecdaquioag@gmail.com>
2017-07-14 13:30:40 -0700

    added year 2015 to the database

--- taxes/years/admin.py
+++ taxes/years/admin.py
@@ -5,5 +5,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Year2013
+from .models import Year2014
+from .models import Year2015
+from .models import Year2016
 
 admin.site.register(Year2013)
+admin.site.register(Year2014)
+admin.site.register(Year2015)
+admin.site.register(Year2016)
--- taxes/years/models.py
+++ taxes/years/models.py
@@ -17,3 +17,43 @@ class Year2013(models.Model):
 
     def __str__(self):
             return self.GeoName + " " + self.Year
+
+class Year2014(models.Model):
+    GeoName = models.CharField(max_length=200)
+    Year = models.CharField(max_length=200)
+    Total_Tax = models.CharField(max_length=200)
+    Property_Tax = models.CharField(max_length=200)
+    Sales_Tax = models.CharField(max_length=200)
+    License_Tax = models.CharField(max_length=200)
+    Income_Tax = models.CharField(max_length=200)
+    Other_Tax = models.CharField(max_length=200)
+
+    def __str__(self):
+            return self.GeoName + " " + self.Year
+
+class Year2015(models.Model):
+    GeoName = models.CharField(max_length=200)
+    Year = models.CharField(max_length=200)
+    Total_Tax = models.CharField(max_length=200)
+    Property_Tax = models.CharField(max_length=200)
+    Sales_Tax = models.CharField(max_length=200)
+    License_Tax = models.CharField(max_length=200)
+    Income_Tax = models.CharField(max_length=200)
+    Other_Tax = models.CharField(max_length=200)
+
+    def __str__(self):
+            return self.GeoName + " " + self.Year
+
+
+class Year2016(models.Model):
+    GeoName = models.CharField(max_length=200)
+    Year = models.CharField(max_length=200)
+    Total_Tax = models.CharField(max_length=200)
+    Property_Tax = models.CharField(max_length=200)
+    Sales_Tax = models.CharField(max_length=200)
+    License_Tax = models.CharField(max_length=200)
+    Income_Tax = models.CharField(max_length=200)
+    Other_Tax = models.CharField(max_length=200)
+
+    def __str__(self):
+            return self.GeoName + " " + self.Year
