1467d784888879e64395d537aae024db8e9ae92e
Regie-Daquioag <regiecdaquioag@gmail.com>
2017-07-17 10:55:52 -0700

    made the base.html for the menu bar

--- taxes/years/urls.py
+++ taxes/years/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # /music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
 ]
--- taxes/years/views.py
+++ taxes/years/views.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+# make a form to create a new object and editing it
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from django.http import HttpResponse
+# this will redirect to whatever link you make it redirect too
+from django.core.urlresolvers import reverse_lazy
+
+from .models import Year2013
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# generic view
+class IndexView(generic.ListView):
+    template_name = 'years/index.html'
+    context_object_name = 'year2013'
+
+    def get_queryset(self):
+        return Year2013.objects.all()
+
+
+# # -*- coding: utf-8 -*-
+# from __future__ import unicode_literals
+#
+# from django.shortcuts import render
+#
+# # Create your views here.
+#
+# from django.http import HttpResponse
+#
+#
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
